0b4efc3357cc6697307f67896083b37fbc3ece23
WBRK-dev <koertjrweberp@gmail.com>
2023-03-21 14:51:37 +0100

    Changed loaded file

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const createWindow = () => {
         autoHideMenuBar: true
     });
 
-    win.loadFile('html/index.html');
+    win.loadFile('html/userCheck/userCheck.html');
 };
 
 app.whenReady().then(() => {
